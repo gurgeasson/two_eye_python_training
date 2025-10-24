131ec92bc79bc4e11eac6238327b223231456d71
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2025-10-24 16:57:50 +0100

    progress

--- 01_Beginner/module_2/excercise/main.py
+++ 01_Beginner/module_2/excercise/main.py
@@ -10,16 +10,41 @@ use functions: eg read_logs(), generate_report()
 upload to Git
 '''
 
+'''
+output template:
+<no of lines> lines of <file name> scanned
+
+--- report ---
+
+<no of finds> critical entries found
+<line/s>
+
+<no of finds> errors found
+<line/s>
+
+report saved to <save name>
+
+'''
+
+
 import glob, os
+
+keywords = ["error", "critical"]
+
+def find_in_file(file_name, search_term):
+    return_data = {"count": 0,
+                    "lines": []}
+    with open(file_name, "r") as file:
+        for line in file:
+            return_data["count"] += 1
+            search_for_error = line.find(search_term)
+            if search_for_error != -1:
+                return_data["lines"].append("Line{}: {}".format(return_data["count"], line.strip()))
+    return return_data
 
 os.chdir("./01_Beginner/module_2/excercise")
 for file in glob.glob("*.txt"):
     print(file)
-    with open(file, "r") as file:
-        count = 0
-        for line in file:
-            count += 1
-            # print("Line{}: {}".format(count, line.strip()))
-            search_for_error = line.find("error")
-            if search_for_error != -1:
-                print(count)
+    for keyword in keywords:
+        for item in find_in_file(file, keyword)["lines"]:
+            print(item)
