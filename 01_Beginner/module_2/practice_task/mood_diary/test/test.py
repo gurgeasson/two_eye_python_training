d43c253246a0183baac76398ea248ef66f0ece20
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2025-10-30 11:55:10 +0000

    progress

--- 01_Beginner/module_2/practice_task/mood_diary/test/test.py
+++ 01_Beginner/module_2/practice_task/mood_diary/test/test.py
@@ -1,24 +1,36 @@
 import unittest
 import os
 
-TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), "../mood_diary" , "2025-10-30.txt")
-
 class TestCalculations(unittest.TestCase):
 
     def setUp(self):
-       self.testfile = open(TESTDATA_FILENAME)
-       self.testdata = self.testfile.read()
+        self.NAME= "Nemo"
+        self.MOOD = "not too bad"
+        TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), "../mood_diary" , "2025-10-30.txt")
+        with open(TESTDATA_FILENAME, "r", encoding="utf-8") as file:
+            self.testdata = file.read()
 
     def tearDown(self):
-       self.testfile.close()
+        pass
 
-    def test_diary_entry_contains_My_name_is(self):
-        key = "My name is"
-        container = self.testdata
-        # error message in case if test case got failed
-        message = "'My name is' is not in diary entry"
-        # assertIn() to check if key is in container
-        self.assertIn(key, container, message)
+    def test_output_is_string(self):
+        self.assertIsInstance(self.testdata, str)
+
+    def test_diary_entry_contains_name(self):
+        self.assertIn(self.NAME, self.testdata, f"'{self.NAME}' is not in diary entry")
+
+    def test_diary_entry_contains_mood(self):
+        self.assertIn(self.MOOD, self.testdata, f"{self.MOOD}' is not in diary entry")
+
+    def test_diary_entry_starts_with_dear_diary(self):
+        KEY = "Dear diary"
+        key_len = len(KEY)
+        first_n_char_of_entry = self.testdata[:key_len]
+        self.assertEqual(KEY, first_n_char_of_entry, f"Diary entry does not start with '{KEY}'")
+        
+        ''' much simpler way:
+        self.assertTrue(self.testdata.startswith("Dear diary"))
+        '''
 
 if __name__ == "__main__":
     unittest.main()
