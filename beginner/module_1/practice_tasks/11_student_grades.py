7f5d9d929b65bd985f6b16c9ea3fd54009bfabe6
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2025-11-13 11:45:07 +0000

    added classes as part of practicing OOP

--- beginner/module_1/practice_tasks/11_student_grades.py
+++ beginner/module_1/practice_tasks/11_student_grades.py
@@ -6,18 +6,50 @@ Instructions
     Create dictionaries for individual students containing "grade" and "DOB".
 '''
 
-student_dictionary = {}
+'''
+pimp my script: to practice OOP I added classes
+'''
 
-alex = {'grade': 88, 'DOB': '10/10/1995'}
-alice = {'grade': 100, 'DOB': '11/05/2003'}
-james = {'grade': 47, 'DOB': '11/09/2013'}
+class Student:
+    def __init__(self, first_name, dob):
+        self.first_name = first_name
+        self.date_of_birth = dob
 
-def add_student(name, info):
-    student_dictionary[name] = info
-    print(f"Student added with key:value pair as {name} : {student_dictionary[name]}")
+    def __str__(self):
+        return f"first name: {self.first_name}, DOB: {self.date_of_birth}"
 
+class Exam:
+    student_dictionary = {}
 
+    def __str__(self):
+        string = "no names in results"
+        if len(self.student_dictionary) > 0:
+            string = ""
+            for i in self.student_dictionary:
+                string += (f"student first_name: {i}, student data: {self.student_dictionary[i]}\n")
+        return string
 
-add_student("Alex", alex)
-add_student("Alice", alice)
-add_student("James", james)
+    def add_student(self, student, grade):
+        self.student_dictionary[student.first_name] = {"grade": grade, "DOB": student.date_of_birth}
+        print(f"Student added with key:value pair as {student.first_name} : {self.student_dictionary[student.first_name]}")
+
+if __name__ == "__main__":
+    alex = Student("Alex", "10/10/1995")
+    alex_grade = 88
+    alice = Student("Alice", "11/05/2003")
+    alice_grade = 100
+    james = Student("James", "11/09/2013")
+    james_grade = 47
+
+    end_of_year_exam_2026 = Exam()
+    
+    print(end_of_year_exam_2026)
+
+    print("adding students to Exam instance:")
+    end_of_year_exam_2026.add_student(alex, alex_grade)
+    end_of_year_exam_2026.add_student(alice, alice_grade)
+    end_of_year_exam_2026.add_student(james, james_grade)
+
+    print("print 'student_dictionary' of my class instance:")
+    print(end_of_year_exam_2026)
+
