f3776e068a1fdc9fe06782dd5cbc6acb59201492
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2026-03-20 15:32:02 +0000

    text changes

--- beginner/module_1/the_great_morning_scramble_with_unittest/scr/game.py
+++ beginner/module_1/the_great_morning_scramble_with_unittest/scr/game.py
@@ -7,15 +7,15 @@ class Game:
         while True:
             if self.count == 3:
                 print("\n" *10)
-                print("too late, you're late... probably best if you call in sick")
+                print("too late, now you're late... probably best that you call in sick")
                 break    
             elif self.count == 0:
-                print("You're late for work and your door key has vanished into thin air!")
+                print("You're late from work and your door key has vanished into thin air!")
             else:
                 print("You're still late, hurry up, you don't have much time left")
             
             if self.locations.search_location(int(input(self.locations.question))):
-                print("Congrats, you found the key. you should be get to work on time today... though before you go out the door. Where are your pants?")
+                print("Congrats, you found the key. you should be on time to work today... though before you go out the door. Where are your pants?")
                 break
 
             self.count += 1
