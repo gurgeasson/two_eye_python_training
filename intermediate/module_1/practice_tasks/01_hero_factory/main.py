524415894d90e22836f02ed2496522d4a735a5cb
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2026-09-16 15:42:57 +0100

    added inheritance

--- intermediate/module_1/practice_tasks/01_hero_factory/main.py
+++ intermediate/module_1/practice_tasks/01_hero_factory/main.py
@@ -8,24 +8,14 @@
 '''
 
 class Superhero:
-    def __init__(self, name, ability, strength, speed, can_fly):
+    def __init__(self, name, ability, strength, speed):
         self.name = name
         self.ability = ability
         self.strength = strength # 1 - 10
         self.speed = speed # 1 - 10
-        self.can_fly = can_fly
-
-    def __str__(self):
-        fly_statement = ""
-        match self.can_fly:
-            case True:
-                fly_statement = 'can fly'
-            case False:
-                fly_statement = 'can\'t fly'
-        return f'{my_superhero.name} is a superhero with a strength of {my_superhero.strength} and a speed of {my_superhero.speed}, who\'s super power is their {my_superhero.ability}. They {fly_statement}.'
 
     def __repr__(self):
-        return f'Superhero data(name="{self.name}", ability="{self.ability}", strength={self.strength}, speed={self.speed}, can_fly={self.can_fly})'
+        return f'Superhero data(name="{self.name}", ability="{self.ability}", strength={self.strength}, speed={self.speed})'
     
     def __add__(self, other):
         if not isinstance(other, Superhero):
@@ -38,13 +28,23 @@ class Superhero:
         yield self.ability
         yield self.strength
         yield self.speed
-        yield self.can_fly
+
+    def __str__(self):
+        return f'{my_superhero.name} is a superhero with a strength of {my_superhero.strength} and a speed of {my_superhero.speed}, who\'s super power is their {my_superhero.ability}.'
 
     def perform_ability(self):
         print(f'{self.name} using their super power, {self.ability}.')
 
+class Flying_hero(Superhero):
+        def __str__(self):
+            return f'{super().__str__()} They can fly.'
+
+class Not_flying_hero(Superhero):
+        def __str__(self):
+            return f'{super().__str__()} They can\'t fly.'
+
 if __name__ == "__main__":
-    my_superhero = Superhero('Samantha Smell' ,'super sense of smell', 3, 2, False)
+    my_superhero = Not_flying_hero('Samantha Smell' ,'super sense of smell', 3, 2)
 
     my_superhero.perform_ability()
     print(my_superhero)
